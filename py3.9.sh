4cb984457bb43d289226998d7d96590b73f66b95
huang wei <491044354@qq.com>
2025-06-30 22:23:08 +0800

    Update py3.9.sh
    1

--- py3.9.sh
+++ py3.9.sh
@@ -1,12 +1,17 @@
 #!/data/data/com.termux/files/usr/bin/bash
-yes | pkg update
-yes | pkg upgrade -y
-pkg install -y git make clang libffi-dev openssl-dev zlib-dev bzip2 libbz2 libsqlite libsqlite-dev
+
+pkg update -y
+pkg install -y git make clang libffi openssl zlib bzip2 libbz2 sqlite
+
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+
 source ~/.bashrc
+
 pyenv install 3.9.18
 pyenv global 3.9.18
+
 python --version
